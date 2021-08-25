1959d0c967ef9bb80702f474353e0e16ff4e0856
yuta <yu.code.tech@gmail.com>
2021-08-25 22:19:04 +0900

    scssファイルの変更

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('aaa');
+console.log("aaa");
 
 //375px 未満は JS で viewport を固定する
 (function () {
@@ -6,38 +6,37 @@ console.log('aaa');
   function switchViewport() {
     const value =
       window.outerWidth > 375
-        ? 'width=device-width,initial-scale=1'
-        : 'width=375';
-    if (viewport.getAttribute('content') !== value) {
-      viewport.setAttribute('content', value);
+        ? "width=device-width,initial-scale=1"
+        : "width=375";
+    if (viewport.getAttribute("content") !== value) {
+      viewport.setAttribute("content", value);
     }
   }
-  addEventListener('resize', switchViewport, false);
+  addEventListener("resize", switchViewport, false);
   switchViewport();
 })();
-
 
 //スクロールアニメーション
 jQuery.fn.acs = function (options) {
   const elements = this;
   const defaults = {
     screenPos: 0.8,
-    className: 'is-animated'
+    className: "is-animated",
   };
   const setting = jQuery.extend(defaults, options);
-  jQuery(window).on('load scroll', function () {
+  jQuery(window).on("load scroll", function () {
     add_class_in_scrolling();
   });
   function add_class_in_scrolling() {
     const winScroll = jQuery(window).scrollTop();
     const winHeight = jQuery(window).height();
-    const scrollPos = winScroll + (winHeight * setting.screenPos);
+    const scrollPos = winScroll + winHeight * setting.screenPos;
 
     if (elements.offset().top < scrollPos) {
       elements.addClass(setting.className);
     }
   }
-}
+};
 jQuery('.anm, [class*="anm-"], .anm-list > *').each(function () {
   jQuery(this).acs();
 });
@@ -45,45 +44,45 @@ jQuery.fn.anmDelay = function (options) {
   const elements = this;
   const defaults = {
     delay: 0.2,
-    property: 'animation-delay'
+    property: "animation-delay",
   };
   const setting = jQuery.extend(defaults, options);
   const index = elements.index();
   const time = index * setting.delay;
-  elements.css(setting.property, time + 's');
-}
-jQuery('.anm-list > *').each(function () {
+  elements.css(setting.property, time + "s");
+};
+jQuery(".anm-list > *").each(function () {
   jQuery(this).anmDelay();
 });
 
 // ページ内スムーズスクロール
 jQuery(function () {
   var windowWidth = jQuery(window).width();
-  var headerHight = 100;//ヘッダー高さ
+  var headerHight = 100; //ヘッダー高さ
   jQuery('a[href^="#"]').click(function () {
-      var speed = 1000;
-      var href = jQuery(this).attr("href");
-      var target = jQuery(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top - headerHight;
-      jQuery('body,html').animate({ scrollTop: position }, speed, 'swing');
-      return false;
+    var speed = 1000;
+    var href = jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - headerHight;
+    jQuery("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
   });
 });
 
 // ハンバーガーメニュー
-jQuery('.menu').on('click', function () {
-  jQuery('.menu__line').toggleClass('active');
-  jQuery('.gnav').fadeToggle();
+jQuery(".js-drawer").on("click", function () {
+  jQuery(".drawer__line").toggleClass("active");
+  jQuery(".drawer-nav").fadeToggle();
 });
-jQuery('.gnav__menu__item a[href]').on('click', function (event) {
-  jQuery('.menu').trigger('click');
+jQuery(".drawer-nav__item a[href]").on("click", function (event) {
+  jQuery(".js-drawer").trigger("click");
 });
 
 //トップへ戻るスクロール検知
-jQuery(window).on("scroll", function() {
+jQuery(window).on("scroll", function () {
   if (400 < jQuery(this).scrollTop()) {
-  jQuery('.to_top').addClass( 'is_show' );
+    jQuery(".js-top").addClass("is-show");
   } else {
-  jQuery('.to_top').removeClass( 'is_show' );
+    jQuery(".js-top").removeClass("is-show");
   }
 });
